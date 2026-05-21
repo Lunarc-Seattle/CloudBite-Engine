08dc3ead323edbfbcf846d0f501f4703e36d0ff3
JIE （FIA） YIN <66171272+Lunarc-Seattle@users.noreply.github.com>
2026-05-21 13:50:00 -0700

    菜品分页查询

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -2,12 +2,15 @@ package com.sky.controller.admin;
 
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.mapper.DishFlavorMapper;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.DishService;
 import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,4 +34,19 @@ public class DishController {
         log.info("新增菜品:{}",dishDTO);
         dishService.saveWithFlavor(dishDTO);
         return  Result.success();
-}}
+    }
+
+    /**
+     * 菜品分页查询
+     */
+    @GetMapping("/page")
+
+    public Result<PageResult> page(DishPageQueryDTO dishPageQueryDTO){
+        log.info("菜品分页查询：{}", dishPageQueryDTO);
+        PageResult pageResult = dishService.pageQuery(dishPageQueryDTO);
+        return Result.success(pageResult);
+    }
+}
+
+
+
